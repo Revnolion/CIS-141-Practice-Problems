527910f02f125c79bb4129df28d76da32c613afa
Evan Johnson <evanjohnson155@gmail.com>
2025-04-27 15:26:42 -0700

    Fixed a indentation issue

--- Module 3/Problem 1.py
+++ Module 3/Problem 1.py
@@ -1,7 +1,7 @@
 # Prompt the user for a word. Then prompt the user for how many times they'd like that work repeated. Use the skills you learned in this module to print the word the correct number of times. 
 
-Word = input("Please input a word, any word: ")
-Repeats = input("How many times would you like this word repeated: ")
-repeat_string = Word * int(Repeats)
+word = input("Please input a word, any word: ")
+repeats = input("How many times would you like this word repeated: ")
+repeat_string = word * int(repeats)
 
 print(repeat_string)
